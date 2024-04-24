642aa83b49fce3d266d03dc4dc0b3b1c9694a59f
Tanishq <tanishqjain1652003@gmail.com>
2024-04-24 17:25:30 +0530

    added bricks and logic to the game

---
+++ MapGenerator.java
@@ -0,0 +1,31 @@
+public class MapGenerator {
+    public int map[][];
+    public int brickWidth;
+    public int brickHeight;
+    public MapGenerator(int row, int col){
+        map = new int[row][col];
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                map[i][j] = 1;
+            }
+        }
+        brickWidth = 540/col;
+        brickHeight = 150/row;
+    }
+    public void draw(java.awt.Graphics2D g){
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                if(map[i][j] > 0){
+                    g.setColor(java.awt.Color.white);
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                    g.setStroke(new java.awt.BasicStroke(3));
+                    g.setColor(java.awt.Color.black);
+                    g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+    public void setBrickValue(int value, int row, int col){
+        map[row][col] = value;
+    }
+}
